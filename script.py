15ca35809c623cbac8fb1afdb823d85e893ae432
WeebNetsu <stephenvdw7777@gmail.com>
2021-02-26 19:27:11 +0200

    SQL Requirements

--- script.py
+++ script.py
@@ -1,3 +1,11 @@
-from app import db
+from app import db, Users
 
 db.create_all()
+
+new_user = Users(username="admin", password="9f3adc4ea3dfdb7f9f42fc4968388304ac8e0b68afa0351e6ab734837f37ebae", mod_=1, admin_=1)
+db.session.add(new_user)  # adds content to database
+db.session.commit()
+
+new_user = Users(username="user", password="d7dbb0f0e3699dcefcce15473f1c2a45c450ab1bb21c937ac3fb1f8d441a1686")
+db.session.add(new_user)  # adds content to database
+db.session.commit()
